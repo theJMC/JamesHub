c3b3532dd76431186e5a3e6200ab28a3cb3afa9c
TheJMC <jamesmccarthy150@gmail.com>
2020-08-05 10:57:31 +0100

    Created the main site

---
+++ server.js
@@ -0,0 +1,58 @@
+// Imports
+const express = require("express");
+const hbs = require('express-handlebars');
+const fs = require('fs');
+const axios = require('axios')
+
+// Settings
+const port = 2000;
+
+// Init Express App
+const app = express();
+
+// Setup handlebars and its settings
+app.set("view engine", "hbs"); 
+app.engine('hbs', hbs({
+    layoutsDir: __dirname + "/views/layouts",
+    extname: 'hbs',
+    defaultLayout: 'index'
+}));
+
+// Serving folders
+app.use(express.static("public")); // Public
+app.use(express.static("plugins")); // Plugins
+
+// |-- -- DEFINING VIEWS -- --| 
+
+
+// Serve the 'main' view
+/*app.get("/", (req, res) => {
+    res.render('dash', {title: "Dashboard", active: { dashboard: true }});
+})
+*/
+app.get("/smarthome", (req, res) => {
+    res.render('smarthome', {title: "Smart Home Control", active: { smarthome: true}})
+})
+
+
+// |-- DEVICES --|
+// Main Hub
+app.get("/", (req, res) => {
+    fs.readFile(__dirname + "/hosts/host.json", "utf-8", (err, data) =>{
+        devices = JSON.parse(data);
+        res.render('devicehub', {title: "Device Hub", active: { devicehub: true}, device: devices})
+    })
+    
+})
+
+// Device Details
+app.get("/device", (req, res) => {
+    var dev = req.query.id;
+    fs.readFile(__dirname + "/hosts/host.json", "utf-8", (err, data) =>{
+        devices = JSON.parse(data);
+        res.render('device', {title: devices[dev]["name"], active: { devicehub: true}})
+    })
+})
+
+// -- START THE SERVER -- 
+app.listen(port, () => console.log(`App is listening on ${port}`));
